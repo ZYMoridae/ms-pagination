8e418e17aecc3af1c3f74414cbc71b25f0a2bfcb
Joe <ashzhouyue@gmail.com>
2019-06-17 21:13:18 +1000

    tweak

---
+++ lib/PaginationComponent.d.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../src/PaginationComponent.css';
+declare type PaginationComponentState = {
+    perPage: number;
+    orderBy: string;
+    page: number;
+};
+declare type PaginationComponentProps = {
+    onPageChanged: (page: number, perPage: number, orderBy: string) => void;
+    count: number;
+    initialPage: number;
+    nextIcon?: React.ReactDOM;
+    previousIcon?: React.ReactDOM;
+};
+declare class PaginationComponent extends React.Component<PaginationComponentProps, PaginationComponentState> {
+    constructor(props: any);
+    updateUrlParmas(page: number, perPage: number, orderBy: string): void;
+    componentWillMount(): void;
+    componentDidMount(): void;
+    pageChanged: (item: number) => void;
+    previousBtnClick: () => void;
+    nextBtnClick: () => void;
+    render(): JSX.Element;
+}
+export default PaginationComponent;
